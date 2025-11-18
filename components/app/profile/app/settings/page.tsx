90fed0a96a64cf8239d03c64a4dbe35f32b4d7ff
NHATDAOKHANG <winnerxx2024@gmail.com>
2025-11-19 01:19:20 +0700

    Add profile page + cleanup files

--- components/app/profile/app/settings/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function SettingsPage() {
-  return (
-    <div className="p-8 text-white">
-      <h1 className="text-2xl font-bold mb-4">Cài đặt</h1>
-      <p>Tính năng cài đặt sẽ được phát triển sau.</p>
-    </div>
-  );
-}
